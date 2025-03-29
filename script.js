08de4e0b09ce38a398908449df67f9a68e28ff42
adamhovda <adamhovda@gmail.com>
2025-03-29 13:34:05 -0700

    added pictures, updated main paage

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-var addDropdown = document.getElementById("serviceDropdown").addEventListener("click", addService);
-var test = document.getElementById("serviceDropdown").addEventListener("click", alert);
 
 
 
-function addService() {
-const sel = document.createElement("select");
-const service1 = document.createElement("option");
-const service2 = document.createElement("option");
+// Email Booking 
 
-service1.value = "manicure";
-service1.text = "Manicure";
 
-service2.value = "pedicure";
-service2.text = "Pedicure";
-
-sel.add(service1, null);
-sel.add(service2, null);
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "mHX_GglA20SntKwJ2",
+    });
+})();
 
 
 
-document.getElementById("bookingForm").appendChild(sel); // put it into the DOM
-
+window.onload = function() {
+    document.getElementById('bookingForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
 }
-
-
-
