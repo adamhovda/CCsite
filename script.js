87aff19c662fab9315f5946e9bb1ee44f0cc6f71
adamhovda <adamhovda@gmail.com>
2025-06-21 22:42:53 -0700

    added bg to about

--- script.js
+++ script.js
@@ -1,46 +1,6 @@
-
-const popup = document.getElementById('myPopup');
-const closeBtn = document.getElementById('closePopup');
 const scrollSections = document.querySelectorAll('.scroll-section');
 
-// Email Booking 
 
-
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-      publicKey: "mHX_GglA20SntKwJ2",
-    });
-})();
-
-
-
-window.onload = function() {
-    document.getElementById('bookingForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(() => {
-                popup.style.display = 'block'; // Show the popup
-                console.log('SUCCESS!');
-            }, (error) => {
-                console.log('FAILED...', error);
-            });
-
-            
-    });
-
-    closeBtn.addEventListener('click', function() {
-        popup.style.display = 'none'; // Hide the popup
-      });
-    
-      // Close the popup if the user clicks outside of it
-      window.addEventListener('click', function(event) {
-        if (event.target === popup) {
-          popup.style.display = 'none';
-        }
-    });
-}
 
 // scrolling code gemini
 
